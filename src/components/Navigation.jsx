3eef70f75bbe40d181c3735edbd2f05700cc6b1c
Curt <cffhome@gmail.com>
2023-12-21 21:32:26 -0600

    Updating files

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -204,7 +204,7 @@ function Search({ placeholder }) {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api/stocks/us');
+      const response = await fetch('http://24.1.70.197:80/api/v1/stocks/us');
       if (!response.ok) throw new Error('Cannot fetch stock data');
       const result = await response.json();
       if (result) stockListRef.current = result;
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      // Your proxy configuration here
-      '/api': {
-        target: 'http://24.1.70.197:80/api/v1', // Replace with your API server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     // Your proxy configuration here
+  //     '/api': {
+  //       target: 'http://24.1.70.197:80/api/v1', // Replace with your API server
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
